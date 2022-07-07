48c05341be9840e0650a6e2bf19ef014a6f800b9
fabralbert <fabrmonk@gmail.com>
2022-07-07 21:47:25 +0300

    added navigate fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 import { Header } from './layout/Header'
@@ -9,41 +9,42 @@ import { Animepage } from './components/Animepage'
 import { Page404 } from './components/Page404'
 import Authorization from './components/Authorization'
 
-
-
-
 function App() {
-
-
-  const [authorization, setAuthorization] = useState(false);
+  const [authorization, setAuthorization] = useState(false)
 
   useEffect(() => {
-    if (localStorage.getItem('authorization') == 1)
-      setAuthorization(true);
-
-  }, []);
-
+    if (localStorage.getItem('authorization') == 1) setAuthorization(true)
+  }, [])
 
   return (
     <>
       <BrowserRouter>
         {authorization ? <Header check={setAuthorization} /> : null}
         <Routes>
-          {authorization ?
+          {authorization ? (
             <>
-              <Route exact path="/" element={<Main />} />
-              <Route path="/favourite" element={<Favourite />} />
-              <Route path="/:animepage/:id" element={<Animepage />} />
+              <Route exact path='/' element={<Main />} />
+              <Route path='/favourite' element={<Favourite />} />
+              <Route path='/:animepage/:id' element={<Animepage />} />
             </>
-            :
-            <Route path="/" element={<Authorization check={setAuthorization} />} />
-          }
-          <Route path="*" element={<Page404 />} />
+          ) : (
+            <>
+              <Route
+                path='/react-anime-search'
+                element={<Navigate replace to='/' />}
+              />
+              <Route
+                path='/'
+                element={<Authorization check={setAuthorization} />}
+              />
+            </>
+          )}
+          <Route path='*' element={<Page404 />} />
         </Routes>
         <Footer />
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
